a420b1002a63636c51dfc0c1c22e70bb53039115
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2023-01-14 11:11:53 +0000

    any type

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,3 +1,16 @@
 let sales= 123_456_789;
 let course = 'Typescript';
 let is_published = true
+let level;
+ 
+// this will return error with type any
+// function render(document: any) {
+//     console.log(document);
+    
+// };
+
+// we can go to tsconfig.js and change "noImplicitAny": false under type checking
+function render(document) {
+    console.log(document);
+    
+}

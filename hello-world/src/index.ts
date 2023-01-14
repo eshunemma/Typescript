49689d778419846d20e64723c306b92c6d1ea93f
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2023-01-14 11:16:01 +0000

    arrays

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,16 +1,3 @@
-let sales= 123_456_789;
-let course = 'Typescript';
-let is_published = true
-let level;
- 
-// this will return error with type any
-// function render(document: any) {
-//     console.log(document);
-    
-// };
+// let numbers: number[] = [1, 2, 3]
 
-// we can go to tsconfig.js and change "noImplicitAny": false under type checking
-function render(document) {
-    console.log(document);
-    
-}
+let numbers = [1, 2, 3]

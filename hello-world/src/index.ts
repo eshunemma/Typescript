2b323bc19dbbe28b8aebd29191429851104ab31a
Emmanuel Eshun <emmanuel.eshun@amalitech.com>
2023-01-15 15:43:51 +0000

    making a parameter or object private

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,19 +1,23 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string;
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(amount: number): void {
         if (amount <= 0)
             throw new Error ('Invalid amount')
-        this.balance += amount;
+        this._balance += amount;
+    }
+
+    getBalance(): number {
+        return this._balance
     }
 }
 
@@ -21,3 +25,5 @@ let account = new Account(1, 'Emma', 0)
 // account.deposit(100)
 // console.log(account.balance);
 // console.log(typeof account);
+
+console.log(account.getBalance());
